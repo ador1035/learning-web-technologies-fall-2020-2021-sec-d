532f86dbea8f501fc4fb307a986bcfc151f70f6a
ador1035 <72723106+ador1035@users.noreply.github.com>
2020-11-05 12:01:50 +0600

    LAB_TASk_4.1_SESSION

---
+++ LAB_TASk_4.1_SESSION/registration.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>XCompany - Home</title>
+</head>
+<body>
+    
+    
+        
+        <table border="2" width="60%">
+            
+            <tr>
+                
+                <td>
+                    
+                    <img src="gallery/logo.png" alt="Logo">
+                    
+                   
+                    
+                </td>
+                
+                <td align="right">
+                    
+                    <a href="#">Home</a>|
+                    <a href="#">Login</a>|
+                    <a href="#">Registration</a>
+                    
+                </td>
+ 
+                
+            </tr>
+            
+            <tr>
+                
+                <td>
+                    
+                    <form action="" method="post">
+                        
+                        <fieldset>
+                            
+                            <legend><b>REGISTRATION</b></legend>
+                            
+                            <br><br>
+                            
+                            Name <pre>            :</pre><input type="text" name="name"><hr>
+                            Email <pre>            :</pre><input type="email" name="email">
+                            <button title="hint:Sample@example.com"><b>i</b></button>
+                            
+                        </fieldset>
+                        
+                    </form>
+                    
+                </td>
+                
+            </tr>
+            
+            <tr>
+                
+                <td colspan="2" align="center"><p>Copyright &#169; 2017</p></td>
+                
+            </tr>
+            
+        </table>
+        
+   
+    
+</body>
+</html>
